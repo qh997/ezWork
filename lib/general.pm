42715878cae49592bca24cd64ca373e23acd24d2
Geng Shuang <gengs@neusoft.com>
2012-04-23 17:17:38 +0800

    添加config文件。

--- lib/general.pm
+++ lib/general.pm
@@ -10,4 +10,23 @@ our $VERSION = qv('0.0.1');
 our @ISA = qw(Exporter);
 our @EXPORT = qw();
 
+my $CONFIG_FILE = 'ezWork.config';
+
+sub get_config {
+    open my $CF, "< $CONFIG_FILE";
+    my @file_content = <$CF>;
+    close $CF;
+
+    my %configs;
+    foreach my $line (@file_content) {
+        chomp $line;
+
+        if ($line =~ m{(.*?)=(.*)}) {
+            $configs{$1} = $2;
+        }
+    }
+
+    return %configs;
+}
+
 return 1;
